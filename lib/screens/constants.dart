22cd3fd3e29a45e00b0540d3ee0c63d0a1064f2d
danielKapash <danielkpsh@gmail.com>
2021-05-21 05:30:54 +0300

    added nutritionist profile

--- lib/screens/constants.dart
+++ lib/screens/constants.dart
@@ -1,50 +1,50 @@
 const NUTRITIONIST_DATA = [
   {
-    "name":"משה כהן",
-    "specialization":"תזונאי בכיר",
-    "price":"5.0",
-    "image":"profile_1.png"
+    "name": "משה כהן",
+    "specialization": "תזונאי בכיר",
+    "price": "5.0",
+    "image": "profile_1.png"
   },
   {
-    "name":"יוני לוי",
-    "specialization":"מתמחה בסכרת",
-    "price":"4.5",
-    "image":"profile_2.png"
+    "name": "יוני לוי",
+    "specialization": "מתמחה בסכרת",
+    "price": "4.5",
+    "image": "profile_2.png"
   },
   {
-    "name":"יונית היימן",
-    "specialization":"מתמחה בילדים",
-    "price":"4.8",
-    "image":"profile_3.png"
+    "name": "יונית היימן",
+    "specialization": "מתמחה בילדים",
+    "price": "4.8",
+    "image": "profile_3.png"
   },
   {
-    "name":".רותם א",
-    "specialization":" ראש עיר",
-    "price":"5.0",
-    "image":"profile_99.png"
+    "name": ".רותם א",
+    "specialization": "מתמחה בטבעונות",
+    "price": "5.0",
+    "image": "profile_99.png"
   },
   {
-    "name":"יוני לוי",
-    "specialization":"מתמחה בסכרת",
-    "price":"4.5",
-    "image":"profile_2.png"
+    "name": "יוני לוי",
+    "specialization": "מתמחה בסכרת",
+    "price": "4.5",
+    "image": "profile_2.png"
   },
   {
-    "name":"יוסי שוורץ",
-    "specialization":"מתמחה בילדים",
-    "price":"4.8",
-    "image":"profile_3.png"
+    "name": "יוסי שוורץ",
+    "specialization": "מתמחה בילדים",
+    "price": "4.8",
+    "image": "profile_3.png"
   },
   {
-    "name":"ניק וייס",
-    "specialization":"מתמחה קיטו",
-    "price":"5.0",
-    "image":"profile_1.png"
+    "name": "ניק וייס",
+    "specialization": "מתמחה קיטו",
+    "price": "5.0",
+    "image": "profile_1.png"
   },
   {
-    "name":"משה כהן",
-    "specialization":"תזונאי בכיר",
-    "price":"5.0",
-    "image":"profile_1.png"
+    "name": "משה כהן",
+    "specialization": "תזונאי בכיר",
+    "price": "5.0",
+    "image": "profile_1.png"
   }
 ];
